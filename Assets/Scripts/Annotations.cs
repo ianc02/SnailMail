c1063b60ba5263cf5188ce7a061a10f67e6f6df2
Josh <thomeczekje@hendrix.edu>
2021-02-13 13:47:04 -0600

    Added all the sprites we'll need... maybe

--- Assets/Scripts/Annotations.cs
+++ Assets/Scripts/Annotations.cs
@@ -6,6 +6,11 @@ public class Annotations : MonoBehaviour
 {
 
     //Tileset: https://finalbossblues.itch.io/tf-jungle-tileset
+    //Beetle: Josh Thomeczek
+    //Envelope: https://opengameart.org/node/108674
+    //Mailbox: https://opengameart.org/content/maibox-sprite-pack
+    //Key: https://opengameart.org/content/key-icons
+    //Lock: https://www.flaticon.com/free-icon/lock_891399
 
     // Start is called before the first frame update
     void Start()
