64996fa19f2ee7a0582db707af90855451775327
ianc02 <77699025+ianc02@users.noreply.github.com>
2021-02-15 14:54:14 -0600

    some beetle movement, not good tho. need rotational forward instead of just going up

--- Assets/Scripts/BeetleMove.cs
+++ Assets/Scripts/BeetleMove.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class BeetleMove : MonoBehaviour
 {
+
+    Rigidbody2D body;
+    public float speed = 10f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        body = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        body.velocity = new Vector2(0, speed);
     }
 }
